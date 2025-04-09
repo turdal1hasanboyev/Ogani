5a6cf34b65da46d12294d1587f9062d2788ce8a9
Turdali <turdalihasanboyev70@gmail.com>
2025-04-09 23:38:02 +0500

    Writing contact model and admin register

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'is_active', 'created_at', 'updated_at',)
+    list_filter = ('is_active',)
+    search_fields = ('name', 'email',)
--- apps/contact/models.py
+++ apps/contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from apps.common.models import BaseModel
 
-# Create your models here.
+
+class Contact(BaseModel):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=100)
+    message = models.TextField(null=True, blank=True) 
+
+    def __str__(self):
+        return f"{self.name}"
